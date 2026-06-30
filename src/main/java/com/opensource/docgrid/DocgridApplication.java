b09bacc04927442b21c29e2fe9b6c3e980f8fc49
kangcheolung <skhu202114111@gmail.com>
2026-06-30 17:15:33 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/opensource/docgrid/DocgridApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.docgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocgridApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opensource/docgrid/DocgridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opensource.docgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocgridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

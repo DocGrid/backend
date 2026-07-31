b41c754b054aaf7f55e9daa23b584a80b5cb9607
김기민 <gimin1463@naver.com>
2026-07-31 14:08:53 +0900

    test: Embedding Client 분리 계약 검증

--- src/test/java/com/opensource/docgrid/domain/embedding/service/query/QueryEmbeddingServiceTest.java
+++ src/test/java/com/opensource/docgrid/domain/embedding/service/query/QueryEmbeddingServiceTest.java
@@ -3,55 +3,37 @@ package com.opensource.docgrid.domain.embedding.service.query;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doReturn;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
-import org.mockito.quality.Strictness;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClient;
 
+import com.opensource.docgrid.domain.embedding.client.EmbeddingClient;
 import com.opensource.docgrid.domain.embedding.dto.EmbedResult;
-import com.opensource.docgrid.domain.embedding.dto.response.EmbedServerResponse;
 import com.opensource.docgrid.domain.embedding.entity.EmbeddingModel;
 import com.opensource.docgrid.domain.embedding.fixture.EmbeddingModelFixture;
 import com.opensource.docgrid.global.exception.DocGridException;
 import com.opensource.docgrid.global.exception.ErrorCode;
 
 @ExtendWith(MockitoExtension.class)
-@MockitoSettings(strictness = Strictness.LENIENT)
 @DisplayName("QueryEmbeddingService 단위 테스트")
 class QueryEmbeddingServiceTest {
 
     @Mock private EmbeddingModelQueryService embeddingModelQueryService;
-    @Mock private RestClient restClient;
-    @Mock(answer = Answers.RETURNS_SELF) private RestClient.RequestBodyUriSpec requestBodyUriSpec;
-    @Mock private RestClient.ResponseSpec responseSpec;
-
-    private QueryEmbeddingService queryEmbeddingService;
-
-    @BeforeEach
-    void setUp() {
-        queryEmbeddingService = new QueryEmbeddingService(embeddingModelQueryService, restClient);
-        doReturn(requestBodyUriSpec).when(restClient).post();
-        doReturn(responseSpec).when(requestBodyUriSpec).retrieve();
-    }
+    @Mock private EmbeddingClient embeddingClient;
+    @InjectMocks private QueryEmbeddingService queryEmbeddingService;
 
     @Test
     @DisplayName("정상 케이스: 텍스트를 1024차원 벡터로 변환한다")
     void embed_success() {
         EmbeddingModel model = EmbeddingModelFixture.createDefaultModel();
         float[] vector = new float[EmbeddingModelFixture.DIMENSION];
-        EmbedServerResponse serverResponse = new EmbedServerResponse(vector);
 
         given(embeddingModelQueryService.getActiveModel()).willReturn(model);
-        given(responseSpec.body(EmbedServerResponse.class)).willReturn(serverResponse);
+        given(embeddingClient.embed("검색어")).willReturn(vector);
 
         EmbedResult result = queryEmbeddingService.embed("검색어");
 
@@ -63,10 +45,9 @@ class QueryEmbeddingServiceTest {
     @DisplayName("차원 불일치: 응답 차원이 모델 차원과 다르면 EMBEDDING_DIMENSION_MISMATCH 예외가 발생한다")
     void embed_dimensionMismatch_throwsException() {
         EmbeddingModel model = EmbeddingModelFixture.createDefaultModel();
-        EmbedServerResponse serverResponse = new EmbedServerResponse(new float[768]);
 
         given(embeddingModelQueryService.getActiveModel()).willReturn(model);
-        given(responseSpec.body(EmbedServerResponse.class)).willReturn(serverResponse);
+        given(embeddingClient.embed("검색어")).willReturn(new float[768]);
 
         assertThatThrownBy(() -> queryEmbeddingService.embed("검색어"))
             .isInstanceOf(DocGridException.class)
@@ -74,16 +55,15 @@ class QueryEmbeddingServiceTest {
     }
 
     @Test
-    @DisplayName("서버 장애: RestClientException 발생 시 EMBEDDING_SERVER_UNAVAILABLE 예외가 발생한다")
-    void embed_serverUnavailable_throwsException() {
+    @DisplayName("빈 응답: Vector가 null이면 EMBEDDING_DIMENSION_MISMATCH 예외가 발생한다")
+    void embed_nullVector_throwsException() {
         EmbeddingModel model = EmbeddingModelFixture.createDefaultModel();
 
         given(embeddingModelQueryService.getActiveModel()).willReturn(model);
-        given(responseSpec.body(EmbedServerResponse.class))
-            .willThrow(new ResourceAccessException("Connection refused"));
+        given(embeddingClient.embed("검색어")).willReturn(null);
 
         assertThatThrownBy(() -> queryEmbeddingService.embed("검색어"))
             .isInstanceOf(DocGridException.class)
-            .hasFieldOrPropertyWithValue("errorCode", ErrorCode.EMBEDDING_SERVER_UNAVAILABLE);
+            .hasFieldOrPropertyWithValue("errorCode", ErrorCode.EMBEDDING_DIMENSION_MISMATCH);
     }
 }
